08f5017de71aa6cded1ca848448639f2d2a1bf0b
Thomas Harvey <tom@alush.co.uk>
2020-01-24 16:50:53 +0100

    initial public release

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='electric_units',
-    version='0.1.3',
+    version='1.0.0',
     description='For handling units of measurement in electricity grid data.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://github.com/ample-tech/electric-units',
     author='ample sustainable brands',
     author_email='administrator@ample.tech',
